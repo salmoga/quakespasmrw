adb49f2cde4653e67fe310595253e4823a98ae79
salmoga <handmadebookclub@mail.com>
2024-09-09 20:56:56 -0400

    Added License Text

--- host.c
+++ host.c
@@ -1,3 +1,25 @@
+/*
+Copyright (C) 1996-2001 Id Software, Inc.
+Copyright (C) 2002-2009 John Fitzgibbons and others
+Copyright (C) 2007-2008 Kristian Duske
+Copyright (C) 2010-2014 QuakeSpasm developers
+Copyright (C) 2024 Edward Meslin
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+*/
+
 /* host.c - Coordinates spawning and killing of local servers */
 
 #include "quakedef.h"
